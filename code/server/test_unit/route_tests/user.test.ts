f476ce47606c2d73c8b7bec251e7a11f451c8ac0
MATTEO GHIA S331347 <s331347@studenti.polito.it>
2024-05-19 15:56:55 +0200

    formatting

--- code/server/test_unit/route_tests/user.test.ts
+++ code/server/test_unit/route_tests/user.test.ts
@@ -4,6 +4,7 @@ import { app } from "../../index"
 
 import UserController from "../../src/controllers/userController"
 const baseURL = "/ezelectronics"
+const usersBaseURL = baseURL + "/users"
 
 //Example of a unit test for the POST ezelectronics/users route
 //The test checks if the route returns a 200 success code
@@ -18,13 +19,15 @@ test("It should return a 200 success code", async () => {
         role: "Manager"
     }
     jest.spyOn(UserController.prototype, "createUser").mockResolvedValueOnce(true) //Mock the createUser method of the controller
-    const response = await request(app).post(baseURL + "/users").send(testUser) //Send a POST request to the route
+    const response = await request(app).post(usersBaseURL).send(testUser) //Send a POST request to the route
     expect(response.status).toBe(200) //Check if the response status is 200
     expect(UserController.prototype.createUser).toHaveBeenCalledTimes(1) //Check if the createUser method has been called once
     //Check if the createUser method has been called with the correct parameters
-    expect(UserController.prototype.createUser).toHaveBeenCalledWith(testUser.username,
+    expect(UserController.prototype.createUser).toHaveBeenCalledWith(
+        testUser.username,
         testUser.name,
         testUser.surname,
         testUser.password,
-        testUser.role)
+        testUser.role
+    )
 })
