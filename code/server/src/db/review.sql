11654b0f392650e54f0542972c72f46899669570
MATTEO GHIA S331347 <s331347@studenti.polito.it>
2024-05-20 12:10:56 +0200

    modified pk in review

--- code/server/src/db/review.sql
+++ code/server/src/db/review.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS review;
 CREATE TABLE review (
-    id INT PRIMARY KEY,
     model TEXT NOT NULL,
     user TEXT NOT NULL,
     score INT NOT NULL CHECK (
@@ -9,6 +8,7 @@ CREATE TABLE review (
     ),
     date DATE NOT NULL,
     comment TEXT NOT NULL,
+    PRIMARY KEY (model, user),
     FOREIGN KEY (model) REFERENCES product(model),
     FOREIGN KEY (user) REFERENCES users(username)
 );
