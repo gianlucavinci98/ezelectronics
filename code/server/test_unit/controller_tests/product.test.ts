b816cf1ff07aa56fbfd62d2934e49eec663a5906
MATTEO GHIA S331347 <s331347@studenti.polito.it>
2024-05-24 11:10:52 +0200

    test: register products param

--- code/server/test_unit/controller_tests/product.test.ts
+++ code/server/test_unit/controller_tests/product.test.ts
@@ -70,3 +70,40 @@ test("register products throws an error if the product already exists", async ()
 
     expect(mock_registerproduct).toHaveBeenCalledTimes(0)
 })
+
+test("register products correctly defaults arrival date to today", async () => {
+    const productController = new ProductController()
+    const product = new Product(100, "model", Category.APPLIANCE, "2020-01-01", "details", 10);
+
+    const mock_getProduct = jest.spyOn(ProductDAO.prototype, "getProduct")
+    mock_getProduct.mockRejectedValueOnce(new ProductNotFoundError())
+
+    const mock_registerProduct = jest.spyOn(ProductDAO.prototype, "registerProduct")
+    mock_registerProduct.mockResolvedValueOnce()
+
+    await expect(
+        productController.registerProducts(
+            product.model,
+            product.category,
+            product.quantity,
+            product.details,
+            product.sellingPrice,
+            null
+        )
+    ).resolves.toBeUndefined()
+
+    expect(mock_getProduct).toHaveBeenCalledTimes(1)
+    expect(mock_getProduct).toHaveBeenCalledWith(product.model)
+
+    const today = new Date().toISOString().split('T')[0]
+
+    expect(mock_registerProduct).toHaveBeenCalledTimes(1)
+    expect(mock_registerProduct).toHaveBeenCalledWith(
+        product.sellingPrice,
+        product.model,
+        product.category,
+        today,
+        product.details,
+        product.quantity
+    )
+})
